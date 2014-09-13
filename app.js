0bd34f0c0f8aab047f57542dd687298d4ef5a88a
Erhan Gündoğan <e.gundogan@salamworld.com>
2014-09-13 21:42:20 +0300

    cors address fixed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('view engine', 'jade');
 app.use(function(req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
