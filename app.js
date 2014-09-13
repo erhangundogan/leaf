3ca8449d59c8caf924d90b4e86f4ecd030da8216
Erhan Gündoğan <e.gundogan@salamworld.com>
2014-09-13 21:32:23 +0300

    cors enabled

--- app.js
+++ app.js
@@ -22,6 +22,26 @@ app.set('port', settings.web.port);
 app.set('views', path.join(__dirname, 'public/templates'));
 app.set('view engine', 'jade');
 
+// Add headers
+app.use(function(req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
